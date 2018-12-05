0aeb8c46ebf012154be87f9d96a82756f8ce4f4c
Hirofumi Yoshikawa <hirofummy@gmail.com>
2018-12-05 23:47:06 +0900

    Hello World!

---
+++ app/src/main/java/com/github/hirofumi/aws_sdk_android_ht_leak/MainActivity.java
@@ -0,0 +1,13 @@
+package com.github.hirofumi.aws_sdk_android_ht_leak;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_main);
+  }
+}
